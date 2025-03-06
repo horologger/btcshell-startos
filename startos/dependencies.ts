493b14a219e8a92afd90f7b3f3fe18cc09590764
horologger <horologger@protonmail.com>
2025-03-06 15:15:40 -0500

    dep healthcheck

--- startos/dependencies.ts
+++ startos/dependencies.ts
@@ -3,7 +3,7 @@ import { sdk } from './sdk'
 export const setDependencies = sdk.setupDependencies(async ({ effects }) => {
   return {
     bitcoind: {
-      healthChecks: [],
+      healthChecks: ['sync-progress'],
       kind: 'running',
       versionRange: '>=28.1',
     },
